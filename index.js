9d5c526728f7f2dd5603d32982c604730b5ed2b7
Otaku.Denka <otaku.denka@gmail.com>
2016-01-24 01:34:29 +0800

    add webpack and angular

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var express = require('express')
 
 var app = express();
 
-app.use(express.static(__dirname + "app"));
+app.use(express.static(__dirname + "/app"));
 
 app.get('/',function(req,res){
-	res.send('Hello')
+	res.sendfile('./app/index.html')
 })
 
 app.listen('3000',function(){
