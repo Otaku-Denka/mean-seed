e272162d8fe75e715d8943b98b0f7a8d98851b4e
Otaku.Denka <otaku.denka@gmail.com>
2016-01-23 21:56:02 +0800

    add server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-test.js
+var express = require('express')
+
+var app = express();
+
+app.use(express.static(__dirname + "app"));
+
+app.get('/',function(req,res){
+	res.send('Hello')
+})
+
+app.listen('3000',function(){
+	console.log('Listening to port 3000')
+})
