094a05294b0da71f2384248643b5219e4a582aeb
Otaku.Denka <otaku.denka@gmail.com>
2016-01-30 02:37:49 +0800

    add production set

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ var express = require('express')
 var app = express();
 
 app.use(express.static(__dirname + "/app"));
+app.use(express.static(__dirname + "/node_modules"))
 
-app.get('/',function(req,res){
-	res.sendfile('./app/index.html')
-})
+// app.get('/',function(req,res){
+// 	res.sendfile('./app/index.html')
+// })
 
 app.listen('3000',function(){
 	console.log('Listening to port 3000')
